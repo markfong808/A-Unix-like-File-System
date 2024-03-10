2135898af9e885ab80672d7156e8cae525bd4b26
Mark <ipo3678w@gamil.com>
2024-03-10 17:53:35 +0000

    done

--- fs.c
+++ fs.c
@@ -75,23 +75,59 @@ i32 fsOpen(str fname) {
   return bfsInumToFd(inum);
 }
 
-
-
 // ============================================================================
 // Read 'numb' bytes of data from the cursor in the file currently fsOpen'd on
 // File Descriptor 'fd' into 'buf'.  On success, return actual number of bytes
 // read (may be less than 'numb' if we hit EOF).  On failure, abort
 // ============================================================================
-i32 fsRead(i32 fd, i32 numb, void* buf) {
+i32 fsRead(i32 fd, i32 numb, void *buf)
+{
+  if (numb < 0)
+    FATAL(ENEGNUMB);
 
-  // ++++++++++++++++++++++++
-  // Insert your code here
-  // ++++++++++++++++++++++++
+  // Get inum and current cursor position from OFT
+  i32 inum = bfsFdToInum(fd);
+  i32 cursor = bfsTell(fd);
+  // printf("Cursor location: %d\n", cursor);
 
-  FATAL(ENYI);                                  // Not Yet Implemented!
-  return 0;
+  // Get the size of the file
+  i32 fileSize = bfsGetSize(inum);
+  //printf("Filesize: %d\n", fileSize);
+
+  // Buffer to store data read from disk
+  i8 readBuf[BYTESPERBLOCK];
+  i32 bytesRead = 0;
+
+  while (bytesRead < numb)
+  {
+    if (cursor + bytesRead >= fileSize)
+      break; // Stop reading if end of file is reached
+
+    // Calculate FBN for current cursor position
+    i32 currentFbn = (cursor + bytesRead) / BYTESPERBLOCK;
+
+    // Translate FBN to DBN
+    i32 dbn = bfsFbnToDbn(inum, currentFbn);
+    if (dbn == ENODBN)
+      break;
+
+    // Read the block from disk
+    bioRead(dbn, readBuf);
+
+    // Determine how many bytes to copy from the block
+    i32 blockOffset = (cursor + bytesRead) % BYTESPERBLOCK;
+    i32 remaining = (numb - bytesRead) < (fileSize - (cursor + bytesRead)) ? (numb - bytesRead) : (fileSize - (cursor + bytesRead));
+    i32 bytesToRead = (BYTESPERBLOCK - blockOffset) < remaining ? (BYTESPERBLOCK - blockOffset) : remaining;
+
+    memcpy((i8 *)buf + bytesRead, readBuf + blockOffset, bytesToRead);
+    bytesRead += bytesToRead;
+  }
+  //  Update cursor position after reading
+  fsSeek(fd, bytesRead, SEEK_CUR);
+
+  // Return the number of bytes actually read
+  return bytesRead;
 }
-
 
 // ============================================================================
 // Move the cursor for the file currently open on File Descriptor 'fd' to the
@@ -161,7 +197,54 @@ i32 fsWrite(i32 fd, i32 numb, void* buf) {
   // ++++++++++++++++++++++++
   // Insert your code here
   // ++++++++++++++++++++++++
+  if (numb < 0)
+    FATAL(ENEGNUMB);
 
-  FATAL(ENYI);                                  // Not Yet Implemented!
+  // Get inum and current cursor position from OFT
+  i32 inum = bfsFdToInum(fd);
+  i32 cursor = bfsTell(fd);
+
+  i32 bytesWritten = 0;
+
+  while (bytesWritten < numb)
+  {
+    // Calculate FBN for current cursor position
+    i32 fbn = (cursor + bytesWritten) / BYTESPERBLOCK;
+
+    // Translate FBN to DBN
+    i32 dbn = bfsFbnToDbn(inum, fbn);
+    if (dbn == ENODBN)
+      dbn = bfsAllocBlock(inum, fbn);
+
+    i32 blockOffset = (cursor + bytesWritten) % BYTESPERBLOCK;
+    i32 bytesInBlock = (BYTESPERBLOCK - blockOffset) < (numb - bytesWritten) ? (BYTESPERBLOCK - blockOffset) : (numb - bytesWritten);
+
+    // Buffer to store data to be written to disk
+    i8 writeBuf[BYTESPERBLOCK];
+
+    // If not writing a full block, read the existing block first
+    if (bytesInBlock < BYTESPERBLOCK)
+      bioRead(dbn, writeBuf);
+
+    // Copy data from the user buffer to the write buffer
+    memcpy(writeBuf + blockOffset, (i8 *)buf + bytesWritten, bytesInBlock);
+
+    // Write the block back to disk
+    bioWrite(dbn, writeBuf);
+
+    // Update the number of bytes written and the cursor position
+    bytesWritten += bytesInBlock;
+  }
+
+  // Update cursor position after writting
+  fsSeek(fd, bytesWritten, SEEK_CUR);
+
+  // Update the file size if necessary
+  i32 newSize = cursor + bytesWritten;
+  if (newSize > bfsGetSize(inum))
+    bfsSetSize(inum, newSize);
+
   return 0;
+  // FATAL(ENYI);                                  // Not Yet Implemented!
+  // return 0;
 }
